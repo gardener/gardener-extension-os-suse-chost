a2d340f0a4844076de519428998a64600f575be1
Tim Usner <tim.usner@sap.com>
2024-05-16 23:44:50 +0200

    Adjust paths after vendor removal

--- charts/gardener-extension-os-suse-chost/doc.go
+++ charts/gardener-extension-os-suse-chost/doc.go
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate sh -c "../../vendor/github.com/gardener/gardener/hack/generate-controller-registration.sh os-suse-chost . $(cat ../../VERSION) ../../example/controller-registration.yaml OperatingSystemConfig:suse-chost OperatingSystemConfig:memoryone-chost"
+//go:generate sh -c "bash $GARDENER_HACK_DIR/generate-controller-registration.sh os-suse-chost . $(cat ../../VERSION) ../../example/controller-registration.yaml OperatingSystemConfig:suse-chost OperatingSystemConfig:memoryone-chost"
 
 // Package chart enables go:generate support for generating the correct controller registration.
 package chart
